6c2ba1f1a7499a6d1b8cde48f426d330f3f4190b
Kaushik Yadala <yadala.k1@gmail.com>
2025-04-12 16:16:54 +0530

    actually commited arduino code

--- Ultrasound_ESP_conf/Ultrasound_ESP_conf.ino
+++ Ultrasound_ESP_conf/Ultrasound_ESP_conf.ino
@@ -4,6 +4,9 @@
 const char* ssid = "KaushikPhone";
 const char* password = "Wifewifi@1";
 
+int trigger = 14;
+int echo = 27;
+
 // Replace with your PC's local IP address where OM2M is running
 const char* om2m_server = "http://172.20.10.3:5089/~/in-cse/in-name/UltrasoundData";
 
@@ -11,39 +14,84 @@ void setup() {
   Serial.begin(115200);
   delay(1000);
 
-  WiFi.begin(ssid, password);
-  Serial.print("Connecting to WiFi...");
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("\nConnected to WiFi!");
+  // setting pins
+  pinMode(trigger, OUTPUT);
+  pinMode(echo, INPUT);
+
+  // WiFi.begin(ssid, password);
+  // Serial.print("Connecting to WiFi...");
+  // while (WiFi.status() != WL_CONNECTED) {
+  //   delay(500);
+  //   Serial.print(".");
+  // }
+  // Serial.println("\nConnected to WiFi!");
   
-  sendDataToOM2M();
+  // sendDataToOM2M();
+
+  String json = buildJson();
+  print(json);
 }
 
-// random data for now
-String getUltrasoundData(){
-   String track_id = "T" + String(random(100, 999));
+float getUltrasoundReading(){
 
+  delayMicroseconds(10);
+  digitalWrite(trigger, LOW);
+  float duration = pulseIn(echo, HIGH);
+  float distance = .5*speed*duration;
+  Serial.println(distance);
+  return distance;
+
+}
+
+String getImage(){
+    // shreyas's job
+}
+
+String checkAlignment(float reading){
+    float threshold = 5;
+    if(reading<threshold){
+        notifyImmediately();
+        return "Bad";
+    }else{
+        return "Good";
+    }
+}
+
+float getTemperature(){
+    // if needed will write the code, for now:
+    return 0;
+}
+
+String buildJson(){
+
+// create a track_id
+   String track_id = "T" + String(999);
+
+// get the time of the reading
   char timeBuffer[20];
   sprintf(timeBuffer, "2025-04-07T%02d:%02d:%02d", random(0, 24), random(0, 60), random(0, 60));
   String time = String(timeBuffer);
 
-  float ultrasound = random(100, 500) / 100.0;    // 1.00 to 5.00 cm
-  float temperature = random(200, 350) / 10.0;    // 20.0 to 35.0 °C
-  float speed = random(20, 100);                 // 20 to 100 km/h
+  // get ultrasound_reading
+  float ultrasound_reading_1 = getUltrasoundReading();    // 1.00 to 5.00 cm
+  float ultrasound_reading_2 = -1;
 
-  const char* conditions[] = {"Defective", "Not-Defective"};
-  String condition = conditions[random(0, 2)];
+  // get image encoding
+  String image_encoding = getImage();
+
+  // get the alignment
+  String alignment = checkAlignment(ultrasound_reading_1);
+
+// get temperature // will probably remove
+  float temperature = getTemperature();    // 20.0 to 35.0 °C
 
   // 👉 Carefully escape inner quotes
   String json = "\\\"track_id\\\":\\\"" + track_id + "\\\",";
   json += "\\\"time\\\":\\\"" + time + "\\\",";
-  json += "\\\"ultrasound_reading\\\":" + String(ultrasound, 2) + ",";
-  json += "\\\"temperature\\\":" + String(temperature, 1) + ",";
-  json += "\\\"speed\\\":" + String(speed, 1) + ",";
-  json += "\\\"track_condition\\\":\\\"" + condition + "\\\"";
+  json += "\\\"ultrasound_reading_1\\\":" + String(ultrasound_reading_1, 2) + ",";
+  json += "\\\"image_encoding\\\":" + image_encoding + ",";
+  json += "\\\"alignment\\\":" + alignment + ",";
+  json += "\\\"temperature\\\":\\\"" + String(temperature, 2) + "\\\"";
 
   json = "{" + json + "}";
 
@@ -61,7 +109,7 @@ void sendDataToOM2M() {
     http.addHeader("Content-Type", "application/vnd.onem2m-res+json; ty=4");
 
     // the data retrieval
-    String sensorData = getUltrasoundData();
+    String sensorData = buildJson();
 
     // the payload
     String payload = "{\"m2m:cin\": {\"con\": \"" + sensorData + "\"}}";
--- computerVisionApi/apiCall.py
+++ computerVisionApi/apiCall.py
@@ -1,6 +1,6 @@
 import APIkey
-from PIL import Image
 from landingai.predict import Predictor
+from PIL import Image
 
 # Enter your API Key
 endpoint_id = "73dadc7b-b774-4791-833f-f51f8b522d93"
@@ -12,7 +12,9 @@ image = Image.open("image.png")
 # Run inference
 predictor = Predictor(endpoint_id, api_key=api_key)
 predictions = predictor.predict(image)
-print(predictions);
+print(predictions)
+print(type(predictions))
+print(predictions[0].label_name)
 
 # output format:
 # one call costs 1 credit
